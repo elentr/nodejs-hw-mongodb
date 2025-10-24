11e844a9a03ab7d8890e4548f0dc56876712a1b5
elentr <tr.13.elena@gmail.com>
2025-10-25 00:44:20 +0300

    add documentation

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import path from 'path';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import contacts from './routers/contacts.js';
@@ -8,11 +9,13 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import usersRouter from './routers/auth.js';
 import { auth } from './middlewares/auth.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 dotenv.config();
 
 // Читаємо змінну оточення PORT
 const PORT = Number(process.env.PORT) || 3000;
+const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
 
 export const setupServer = () => {
   const app = express();
@@ -29,6 +32,8 @@ export const setupServer = () => {
   app.use(cookieParser());
   app.use('/auth', usersRouter);
   app.use('/contacts', auth, contacts);
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
   app.use(notFoundHandler);
   app.use(errorHandler);
 
